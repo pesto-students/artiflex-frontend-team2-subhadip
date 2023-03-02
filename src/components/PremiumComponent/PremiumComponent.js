769879b1222d4346fbaf10330eb2adda00e90543
Akash <akashmm92@gmail.com>
2023-03-02 18:44:51 +0530

    premium card added

--- src/components/PremiumComponent/PremiumComponent.js
+++ src/components/PremiumComponent/PremiumComponent.js
@@ -1,19 +1,22 @@
 import React from "react";
-import { Row, Col } from "react-grid-system";
+import { Container, Row, Col } from "react-grid-system";
 import "./PremiumComponent.css";
-// import PrimaryButtonComp from "../PrimaryButtonComponent/PrimaryButtonComponent";
-import { colorPalette } from '@cred/neopop-web/lib/primitives';
-
+import PrimaryButtonComp from "../PrimaryButtonComponent/PrimaryButtonComponent";
 
 import {
-    ElevatedCard,
-    Typography,
-    Button,
+  ElevatedCard,
+  Typography,
+  HorizontalSpacer,
+  Tag,
+  Button,
 } from "@cred/neopop-web/lib/components";
 
 import {
-    mainColors,
-    fontNameSpaces,
+  mainColors,
+  colorPalette,
+  colorGuide,
+  fontNameSpaces,
+  getButtonConfig,
 } from "@cred/neopop-web/lib/primitives";
 
 import styled from "styled-components";
@@ -22,51 +25,43 @@ const ContentWrapper = styled.div`
   padding: 20px;
 `;
 
-const PremiumCardComponent = () => {
-    return (
-        <ElevatedCard
-
-            borderColor="white"
-            backgroundColor="#1a1a1b"
-            edgeColors={{
-                right: colorPalette.success[200],
-                bottom: colorPalette.success[200],
-            }}
-            style={{
-                // width: '426px',
-                height: "163px",
-            }}
-        >
-            <ContentWrapper>
-                <Row>
-                    <Col>
-                        <Typography
-                            className="title"
-                            {...fontNameSpaces.tc12b}
-                            color={mainColors.white}
-                            fontSize={25}
-                        >
-                            Premium
-                        </Typography>
-                    </Col>
-                    <Col className="diamond"></Col>
-                </Row>
-                <Button
-                    variant="secondary"
-                    kind="elevated"
-                    fullWidth="true"
-                    colorConfig={{
-                        backgroundColor: "#67FF88",
-                        edgeColors: { right: "#FFFAE5", bottom: "#FFFAE5" },
-                        borderColor: "black",
-                        color: "black",
-                    }}
-                >
-                    Try Now
-                </Button>
-            </ContentWrapper>
-        </ElevatedCard>
-    );
+const Card = () => {
+  return (
+    <ElevatedCard
+      backgroundColor="#1a1a1b"
+      edgeColors={{
+        bottom: "white",
+        right: "white",
+      }}
+      style={{
+        width: "426px",
+        height: "163px",
+      }}
+    >
+      <ContentWrapper>
+        <Row>
+          <Col>
+            <Typography
+              {...fontNameSpaces.tc12b}
+              color={mainColors.white}
+              fontSize={25}
+            >
+              Premium
+            </Typography>
+          </Col>
+          <Col className="diamond"></Col>
+        </Row>
+        <Row>
+          <PrimaryButtonComp
+            size="medium"
+            text="Try Now"
+            color="black"
+            backgroundColor="white"
+          />
+        </Row>
+      </ContentWrapper>
+    </ElevatedCard>
+  );
 };
 
-export default PremiumCardComponent;
+export default Card;
