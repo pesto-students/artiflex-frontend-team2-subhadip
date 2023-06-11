0240a0e4b40a5e08daf25deef2b55809b9c18115
Jimish Gajjar <29489233+jimishgajjar@users.noreply.github.com>
2023-06-11 20:17:15 +0530

    Revert "Feature/firebase image upload"

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,30 +7,3 @@ export const LandingPage = React.lazy(() =>
 export const SomeOtherPage = React.lazy(() =>
   import("./SomeOtherPage/SomeOtherPage")
 );
-
-export const RegisterPage = React.lazy(() =>
-  import("./RegisterPage/RegisterPage")
-);
-export const MainPage = React.lazy(() => import("./MainPage/MainPage"));
-
-export const LoginPage = React.lazy(() => import("./LoginPage/LoginPage"));
-
-export const TimeLinePage = React.lazy(() =>
-  import("./TimeLinePage/TimeLinePage")
-);
-
-export const AddPostFormPage = React.lazy(() =>
-  import("./AddPostFormPage/AddPostFormPage")
-);
-
-export const ViewPostPage = React.lazy(() =>
-  import("./ViewPostPage/ViewPostPage")
-);
-
-export const PremiumPage = React.lazy(() =>
-  import("./PremiumPage/PremiumPage")
-);
-
-export const ProfilePage = React.lazy(() =>
-  import("./ProfilePage/ProfilePage")
-);
