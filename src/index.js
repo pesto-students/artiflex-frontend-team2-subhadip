81d990afe98796128bf5cbf1a52f1f7f77a0c7f2
Akash <akashmm92@gmail.com>
2023-07-11 21:35:50 +0530

    redux

--- src/index.js
+++ src/index.js
@@ -7,10 +7,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 
+import { Provider } from "react-redux"; // Import Provider from react-redux
+import { store } from "./redux/store"; // Import your Redux store
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,6 +1,9 @@
-import React from "react";
+// import React from "react";
 import "./ProfilePage.css";
-import { Button } from "@cred/neopop-web/lib/components";
+import React, { useCallback } from "react";
+
+// import { Button } from "@cred/neopop-web/lib/components";
+// import { store } from "../../redux/store";
 
 import TopNavComponent from "../../components/TopNavComponent/TopNavComponent";
 import AnimCursorComponent from "../../components/AnimCursorComponent/AnimCursorComponent";
@@ -8,9 +11,45 @@ import ProfileInfoComponent from "../../components/ProfileInfoComponent/Profilei
 import PremiumCardComponent from "../../components/PremiumComponent/PremiumComponent";
 import RecentPostCardComponent from "../../components/RecentPostComponent/RecentPostComponent";
 import { Container, Row, Col } from "react-grid-system";
-// import PostsListComponent from "../../components/PostsListComponent/PostsListComponent";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { getPostBasedOnUser } from "../../redux/post/postSlice";
+
+import PostsListComponent from "../../components/PostsListComponent/PostsListComponent";
 
 const ProfilePage = () => {
+  const dispatch = useDispatch();
+
+  const authState = useSelector((state) => state.auth.data.user._id);
+
+  const [allPosts, setAllPosts] = React.useState([]);
+
+  const allPostsBasedOnUser = useCallback(
+    async (payload = { user_id: "" }) =>
+      dispatch(getPostBasedOnUser(payload)).unwrap(),
+    []
+  );
+
+  const loadPostBasedOnUser = async () => {
+    try {
+      const payload = { user_id: authState };
+
+      const res = await allPostsBasedOnUser(payload);
+      console.dir(res);
+
+      // stopLoading();
+      setAllPosts(res);
+    } catch (error) {
+      // stopLoading();
+    }
+  };
+
+  React.useEffect(() => {
+    loadPostBasedOnUser();
+  }, []);
+
+  // console.log(allPosts);
+
   return (
     <>
       <AnimCursorComponent />
@@ -30,7 +69,7 @@ const ProfilePage = () => {
         <Row>
           <Col sm={1} md={2}></Col>
           <Col sm={12} xs={12} md={6} className="post-list-container">
-            {/* <PostsListComponent /> */}
+            <PostsListComponent allPostsArr={allPosts} />
           </Col>
           <Col sm={1} md={1}></Col>
           <Col sm={12} md={3} className="recentpost">
